42591bf0cdb029323a80e9cc9c567d4e5e4ac372
Masoud Rahmani <mrem71@gmail.com>
2023-05-20 22:17:18 +0330

    WdSize-DragDrop-Converter-DestinationSetting

---
+++ WordRename/EnableToVisibleConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace WordRename
+{
+    [ValueConversion(typeof(Boolean), typeof(Visibility))]
+    internal class EnableToVisibleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value) { return Visibility.Hidden; }
+            else { return Visibility.Visible; }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var d = (Visibility)value;
+            if (d == Visibility.Hidden) { return false; } else { return true; }
+        }
+    }
+}
